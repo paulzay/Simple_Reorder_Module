a40388cdab138e1da09472c82a9a528cdbaf36f2
Paul Ogolla <paulotieno2@gmail.com>
2021-08-27 12:02:42 +0300

    create a tab for the warehouse view

---
+++ src/components/Warehouse.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Warehouse() {
+  return (
+    <div className="warehouse">
+      <div className="pending">
+        <h2>Pending Orders</h2>
+      </div>
+      <div className="processed">
+        <h2>Processed Orders</h2>
+      </div>
+    </div>
+  )
+}
