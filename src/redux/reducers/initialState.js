0bdf818d4cedfb9926a74f5425c5c1c01dcbd91a
Paul Ogolla <paulotieno2@gmail.com>
2021-08-27 11:54:21 +0300

    setup redux for global state of products and reconfig project, install dependencies

---
+++ src/redux/reducers/initialState.js
@@ -0,0 +1,39 @@
+export default {
+  products: [
+  {
+    id: 1,
+    name: "Rice",
+    imageURL: "",
+    quantity: 100,
+    price: 20,
+  },
+  {
+    id: 2,
+    name: "Cooking Oil",
+    imageURL: "",
+    quantity: 60,
+    price: 20,
+  },
+  {
+    id: 3,
+    name: "Maize FLour",
+    imageURL: "",
+    quantity: 40,
+    price: 20,
+  },
+  {
+    id: 4,
+    name: "Wheat FLour",
+    imageURL: "",
+    quantity: 40,
+    price: 20,
+  },
+  {
+    id: 5,
+    name: "Maize FLour",
+    imageURL: "",
+    quantity: 40,
+    price: 20,
+  },
+],
+};
---
+++ src/redux/reducers/products.js
@@ -0,0 +1,8 @@
+import initialState from './initialState';
+
+export default function products(state = initialState.products, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
